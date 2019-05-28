ebae5dc9ae662b14685af14b689ab83ff13c3195
hiratafs <tatihirata@outlook.com>
2019-05-28 10:40:38 -0400

    Retirado o Link do import

--- src/AdicionaLivro.js
+++ src/AdicionaLivro.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react' 
-//import { Link } from 'react-router-dom'
 
 
 class AdicionaLivro extends Component {
     render () {
         return (
+        
         <div className="open-search">
-            <a href="/listalivros">Adicionar livro</a>
+            <a href="/busca">Adicionar livro</a>
         </div>
         )
     }
