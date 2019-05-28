16a85c78afd36d04c5e505c89c9052ec902f4615
hiratafs <tatihirata@outlook.com>
2019-05-28 10:43:20 -0400

    Adicionado Link para o botão de retornar

--- src/BotaoRetorna.js
+++ src/BotaoRetorna.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class BotaoRetorna extends Component {
     render () {
         return(
 
-            <button className="close-search" >Close</button>
+            <Link to='/' className="close-search" >Close</Link>
 
         )
     }
