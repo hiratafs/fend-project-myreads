6976eec0d8326a0e3b07e8a37912a0e7779b819b
hiratafs <tatihirata@outlook.com>
2019-05-29 14:12:29 -0400

    Colocado defaultvalue para marcar a estante atual de cada livro

--- src/StatusLivro.js
+++ src/StatusLivro.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react'
 
 class StatusLivro extends Component  {
 
-    moveEstante = (e) => {
-        console.log(e.target.value)
-    }
 
     render () {
         return (
             <div className="book-shelf-changer">
                 <select onChange={(e) => this.props.atualizaLista(
-                    this.props.livro, e.target.value)} value={this.props.estante}>
-                    <option value="move" disabled>Mover para...</option>
-                    <option value="currentlyReading">Lendo</option>
+                    this.props.livro, e.target.value)} defaultValue={this.props.estanteatual}>
+                    <option value="move" onClick={this.marcaEstante} disabled>Mover para...</option>
+                    <option value="currentlyReading" >Lendo</option>
                     <option value="wantToRead">Quero Ler</option>
                     <option value="read">Lido</option>
                     <option value="none">None</option>
