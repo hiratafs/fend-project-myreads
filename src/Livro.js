fa71c463d183bd1f81d1ce5af2e7861a1be75468
hiratafs <tatihirata@outlook.com>
2019-05-29 14:10:45 -0400

    Colocada condição para capa do livro

--- src/Livro.js
+++ src/Livro.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import StatusLivro from './StatusLivro'
+import capaIndisponivel from './img/sem_capa.png'
 
 class Livro extends Component  {
-  
+
     render () {
+
+        const capaLivro = this.props.livro.imageLinks && this.props.livro.imageLinks.thumbnail ? this.props.livro.imageLinks.thumbnail : capaIndisponivel;
+
         return (
             <li>
             <div className="book">
                 <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.livro.imageLinks.thumbnail})` }}></div>
-                    <StatusLivro livro={this.props.livro} atualizaLista={this.props.atualizaLista}/>
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${capaLivro})` }}></div>
+                    <StatusLivro livro={this.props.livro} atualizaLista={this.props.atualizaLista} estanteatual={this.props.estanteatual}/>
                 </div>
                 <div className="book-title">{this.props.livro.title}</div>
                 <div className="book-authors">{this.props.livro.authors}</div>
