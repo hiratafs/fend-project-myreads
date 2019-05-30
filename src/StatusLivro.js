53b1684ae7d13ab2db45bee6e4426ccb357e3010
hiratafs <tatihirata@outlook.com>
2019-05-30 11:46:18 -0400

    Inserida lógica para definição do defaultValue

--- src/StatusLivro.js
+++ src/StatusLivro.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 
 class StatusLivro extends Component  {
-
-
     render () {
+        let estantepadrao = 'none'
+
+        for(let volume of this.props.livros) {
+            if(volume.id === this.props.livro.id) {
+                estantepadrao = this.props.livro.shelf
+            }
+        }
+
         return (
             <div className="book-shelf-changer">
                 <select onChange={(e) => this.props.atualizaLista(
-                    this.props.livro, e.target.value)} defaultValue={this.props.estanteatual}>
+                    this.props.livro, e.target.value)} defaultValue={estantepadrao}>
                     <option value="move" onClick={this.marcaEstante} disabled>Mover para...</option>
                     <option value="currentlyReading" >Lendo</option>
                     <option value="wantToRead">Quero Ler</option>
