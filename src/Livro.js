4cb4e743926ed8314429179185a5b7b46d91500d
hiratafs <tatihirata@outlook.com>
2019-05-30 11:49:46 -0400

    Retirado estanteatual do props

--- src/Livro.js
+++ src/Livro.js
@@ -13,7 +13,7 @@ class Livro extends Component  {
             <div className="book">
                 <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${capaLivro})` }}></div>
-                    <StatusLivro livro={this.props.livro} atualizaLista={this.props.atualizaLista} estanteatual={this.props.estanteatual}/>
+                    <StatusLivro livro={this.props.livro} livros={this.props.livros} atualizaLista={this.props.atualizaLista} estanteatual={this.props.estanteatual}/>
                 </div>
                 <div className="book-title">{this.props.livro.title}</div>
                 <div className="book-authors">{this.props.livro.authors}</div>
